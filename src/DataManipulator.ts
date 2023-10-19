72e507fee6b69f71a7c1fe6a53a8bc8f2adca457
Chitranshu511 <chitranshu511@gmail.com>
2023-10-19 17:18:06 +0530

    here is changes

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -4,17 +4,45 @@ export interface Row {
   stock: string,
   top_ask_price: number,
   timestamp: Date,
+  price_abc: number, // Add 'price_abc' field
+  price_def: number, // Add 'price_def' field
+  ratio: number, // Add 'ratio' field
+  lower_bound: number, // Add 'lower_bound' field
+  upper_bound: number, // Add 'upper_bound' field
+  trigger_alert: number, // Add 'trigger_alert' field
 }
-
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
     return serverResponds.map((el: any) => {
+      // Calculate 'price_abc' and 'price_def' as in Task 1
+      const price_abc = el.top_ask && el.top_ask.price || 0;
+      const price_def = el.top_bid && el.top_bid.price || 0;
+
+      // Calculate the 'ratio'
+      const ratio = price_abc / price_def;
+
+      // Set 'lower_bound' and 'upper_bound' (adjust these values as needed)
+      const lower_bound = ratio * 0.95; // Example value, adjust as needed
+      const upper_bound = ratio * 1.05; // Example value, adjust as needed
+
+      // Determine 'trigger_alert' based on whether the ratio crosses the bounds
+      let trigger_alert = undefined;
+      if (ratio > upper_bound || ratio < lower_bound) {
+        trigger_alert = ratio;
+      }
+
       return {
         stock: el.stock,
         top_ask_price: el.top_ask && el.top_ask.price || 0,
         timestamp: el.timestamp,
+        price_abc,
+        price_def,
+        ratio,
+        lower_bound,
+        upper_bound,
+        trigger_alert,
       };
-    })
+    });
   }
 }
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -5,28 +5,28 @@ import { DataManipulator } from './DataManipulator';
 import './Graph.css';
 
 interface IProps {
-  data: ServerRespond[],
+  data: ServerRespond[];
 }
 
 interface PerspectiveViewerElement extends HTMLElement {
-  load: (table: Table) => void,
+  load: (table: Table) => void;
 }
+
 class Graph extends Component<IProps, {}> {
   table: Table | undefined;
-
-  render() {
-    return React.createElement('perspective-viewer');
-  }
 
   componentDidMount() {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+      ratio: 'float',  // Add the 'ratio' field
+      upper_bound: 'float',  // Add the 'upper_bound' field
+      lower_bound: 'float',  // Add the 'lower_bound' field
+      trigger_alert: 'float',  // Add the 'trigger_alert' field
+      price_abc: 'float',  // Add 'price_abc' field
+      price_def: 'float',  // Add 'price_def' field
+      timestamp: 'date',  // Add the 'timestamp' field
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -36,24 +36,33 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
+        ratio: 'avg',  // Update the aggregate for 'ratio'
+        lower_bound: 'last',  // Update the aggregate for 'lower_bound'
+        upper_bound: 'last',  // Update the aggregate for 'upper_bound'
+        trigger_alert: 'last',  // Update the aggregate for 'trigger_alert'
+        price_abc: 'avg',  // Update the aggregate for 'price_abc'
+        price_def: 'avg',  // Update the aggregate for 'price_def'
+        timestamp: 'distinct count',  // Update the aggregate for 'timestamp'
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
+      // Update the table with the new data
       this.table.update(
-        DataManipulator.generateRow(this.props.data),
+        DataManipulator.generateRow(this.props.data)
       );
     }
+  }
+
+  render() {
+    return (
+      <perspective-viewer />
+    );
   }
 }
 
